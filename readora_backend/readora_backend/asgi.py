fee1bf6f40efc002020718aa4a6adfb3868e24b9
raorajnish <rajnish.18310@sakec.ac.in>
2026-03-27 20:37:54 +0530

    fix: resolve asgi initialization and centralize frontend urls

--- readora_backend/readora_backend/asgi.py
+++ readora_backend/readora_backend/asgi.py
@@ -1,16 +1,20 @@
 import os
 from django.core.asgi import get_asgi_application
+
+# Set settings before anything else
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'readora_backend.settings')
+
+# Initialize Django ASGI application early
+django_asgi_app = get_asgi_application()
+
+# Now import channels and routing
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from chat.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'readora_backend.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
+        URLRouter(websocket_urlpatterns)
     ),
 })
